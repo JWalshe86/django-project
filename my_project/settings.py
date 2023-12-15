aec666ef9556204f0eb47fd82c35ee3ad50ad44a
John Walshe <walshejohnnyw7@gmail.com>
2023-12-15 15:30:10 +0000

    add prep for heroku deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(ilre&9+ozwt=a3yyz!ym(085z4i46a@=uz+@0!1^fx+#9n2ik'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokuapp.com']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello_world',
+    'about',
 ]
 
 MIDDLEWARE = [
